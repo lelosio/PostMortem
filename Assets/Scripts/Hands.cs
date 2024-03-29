f8e7002d34e5bfe5d41870267921bcf93d151092
arCnuts <111523660+arCnuts@users.noreply.github.com>
2024-03-29 15:41:24 +0100

    +cycling +empty clip sound +1/2 gunshot

--- Assets/Scripts/Hands.cs
+++ Assets/Scripts/Hands.cs
@@ -1,8 +1,9 @@
 using UnityEditor.Animations;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
-using System;
+using FMOD;
+using FMODUnity;
+using FMOD.Studio;
 
 public class Hands : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class Hands : MonoBehaviour
 
     public AnimatorController pistolAnim;
     public AnimatorController shotgunAnim;
+    public Animator crosshair;
     public Animator hand;
     public Gun[] Inventory = new Gun[4];
     public Gun equippedGun;
@@ -29,17 +31,18 @@ public class Hands : MonoBehaviour
     public Light muzzleFlashFlash;
     public ParticleSystem gunImpact;
 
+    public EventReference gunShot;
+    public EventReference emptyClip;
+
     int selectedGun;
     private float flashIntensity;
     private float lightIntensity;
 
-    // OSKAR
-    //==============
     public TextMeshProUGUI ammoUI;
-    bool reloading;                                                          // UNUSED BOOLS?
+    bool reloading;
     bool readyToShoot;
 
-    public struct Gun
+    public class Gun
     {
         public string name;
         public int ammo;
@@ -48,11 +51,11 @@ public class Hands : MonoBehaviour
         public AnimatorController ac;
         public bool acquired;
         public int bulletsLeft;
-        public int bulletsShot;
+        public int pellets;
         public float shootingCooldown;
         public float reloadTime;
 
-        public Gun(string Name, int Ammo, int MagSize, float Spread, float ReloadTime, float ShootingCooldown, AnimatorController AC, bool Acquired)
+        public Gun(string Name, int Ammo, int MagSize, float Spread, float ReloadTime, float ShootingCooldown, AnimatorController AC, bool Acquired, int Pellets = 1)
         {
             name = Name;
             ammo = Ammo;
@@ -64,64 +67,29 @@ public class Hands : MonoBehaviour
             ac = AC;
             acquired = Acquired;
         
-            bulletsShot = 0;
             bulletsLeft = magSize;
+
+            pellets = Pellets;
         }
     }
     
     void Start()
     {
-        Gun pistol = new Gun("Pistol", 64, 12, 1.2f, 0.5f, 1f, pistolAnim, true);
-        Gun shotgun = new Gun("Shotgun", 32, 6, 1.2f, 0.5f, 1f, shotgunAnim, true);
-        Gun submachineGun = new Gun("Pistol", 200, 12, 1.2f, 0.5f, 1f, null, false);
-        Gun machineGun = new Gun("Pistol", 200, 12, 1.2f, 0.5f, 1f, null, false);
+        Gun pistol = new Gun("Pistol", 24, 12, 0f, 0.25f, 1f, pistolAnim, true);
+        Gun shotgun = new Gun("Shotgun", 10, 3, 0.1f, 0.5f, 0.3f, shotgunAnim, true, 6);
+        Gun submachineGun = new Gun("Pistol", 300, 12, 1.2f, 0.5f, 1f, null, false);
+        Gun machineGun = new Gun("Pistol", 400, 12, 1.2f, 0.5f, 1f, null, true);
 
         Inventory[0] = pistol;
         Inventory[1] = shotgun;
         Inventory[2] = submachineGun;
         Inventory[3] = machineGun;
 
-        // for (int i = 0; i < Inventory.Length; i++)
-        // {
-        //     Inventory[i].bulletsLeft = Inventory[i].magSize;
-        // }
         readyToShoot = true;
     }
 
     void Update()
     {
-        // ammoUI.SetText(equippedGun.bulletsLeft + "/" + equippedGun.ammo);
-
-        equippedGun = Inventory[selectedGun];
-        selectedGun += (int)Input.mouseScrollDelta.y;
-        if(Input.GetKeyDown(KeyCode.Alpha1)) selectedGun = 0;
-        if(Input.GetKeyDown(KeyCode.Alpha2)) selectedGun = 1;
-        if(Input.GetKeyDown(KeyCode.Alpha3)) selectedGun = 2;
-        if(Input.GetKeyDown(KeyCode.Alpha4)) selectedGun = 3;
-        selectedGun = Mathf.Clamp(selectedGun, 0, Inventory.Length - 1);
-        int originalSelectedGun = selectedGun;
-        while (!Inventory[selectedGun].acquired)
-        {
-            selectedGun++;
-            if (selectedGun >= Inventory.Length) selectedGun = 0;
-            if (selectedGun == originalSelectedGun) break;
-        }
-        hand.runtimeAnimatorController = equippedGun.ac;
-
-        flashIntensity = muzzleFlashFlash.intensity;
-        lightIntensity = muzzleFlashLight.intensity;
-
-        if (Input.GetKeyDown(KeyCode.R) && equippedGun.bulletsLeft < equippedGun.magSize && !reloading) Reload();
-        if (Input.GetMouseButtonDown(0) && equippedGun.bulletsLeft > 0 && readyToShoot)
-        {
-            Shoot();
-        }
-
-        muzzleFlashLight.intensity = Mathf.Lerp(lightIntensity,0f, Time.deltaTime * 10f);
-        muzzleFlashFlash.intensity = Mathf.Lerp(flashIntensity,0f, Time.deltaTime * 15f);
-
-        ammoUI.text = equippedGun.bulletsLeft.ToString();
-
         #region  Camera Sway
         xMousePos = Input.GetAxisRaw("Mouse X");
         yMousePos = Input.GetAxisRaw("Mouse Y");
@@ -133,6 +101,56 @@ public class Hands : MonoBehaviour
         
         // targetPosition += new Vector3(1000f,1000f, 10f);    SUPPOSED TO BE IN SHOOT METHOD
         #endregion
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && Inventory[0].acquired) selectedGun = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2) && Inventory[1].acquired) selectedGun = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3) && Inventory[2].acquired) selectedGun = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha4) && Inventory[3].acquired) selectedGun = 3;
+        equippedGun = Inventory[selectedGun];
+        int nextSelectedGun = selectedGun + (int)Input.mouseScrollDelta.y;
+        if (nextSelectedGun < 0) {
+            nextSelectedGun = Inventory.Length - 1;
+        } else if (nextSelectedGun >= Inventory.Length) {
+            nextSelectedGun = 0;
+        }
+
+        while (!Inventory[nextSelectedGun].acquired) {
+            nextSelectedGun += (int)Input.mouseScrollDelta.y;
+            if (nextSelectedGun < 0) {
+                nextSelectedGun = Inventory.Length - 1;
+            } else if (nextSelectedGun >= Inventory.Length) {
+                nextSelectedGun = 0;
+            }
+        }
+    
+
+        selectedGun = nextSelectedGun;
+
+
+        hand.runtimeAnimatorController = equippedGun.ac;
+
+        flashIntensity = muzzleFlashFlash.intensity;
+        lightIntensity = muzzleFlashLight.intensity;
+
+        if (Input.GetKeyDown(KeyCode.R) && equippedGun.bulletsLeft < equippedGun.magSize && !reloading) Reload();
+        if (Input.GetMouseButtonDown(0) && readyToShoot && !reloading)
+        {
+            if (equippedGun.bulletsLeft > 0)
+            {
+                Shoot();
+            }
+            else
+            {
+                readyToShoot = false;
+                RuntimeManager.PlayOneShot(emptyClip);
+                Invoke("ResetShot", equippedGun.shootingCooldown);
+            }
+        }
+
+        muzzleFlashLight.intensity = Mathf.Lerp(lightIntensity,0f, Time.deltaTime * 10f);
+        muzzleFlashFlash.intensity = Mathf.Lerp(flashIntensity,0f, Time.deltaTime * 15f);
+
+        ammoUI.SetText(equippedGun.bulletsLeft + "/" + equippedGun.ammo);
     }
 
     void Shoot()
@@ -141,29 +159,34 @@ public class Hands : MonoBehaviour
 
         muzzleFlashAnimator.Play("flashOn");
         hand.Play("gunShoot");
+        crosshair.Play("crosshairShot");
         CameraShaker.Invoke();
         
         flashIntensity = 0.3f;
         lightIntensity = 300f;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        RuntimeManager.PlayOneShot(gunShot);
+        
+        for (int i = 0; i < equippedGun.pellets; i++)
         {
-            gunImpact.transform.position = hit.point;
-            gunImpact.transform.forward = hit.normal;
-            gunImpact.Emit(1);
+            float y = Random.Range(-equippedGun.spread, equippedGun.spread);
+            float x = Random.Range(-equippedGun.spread, equippedGun.spread);
 
-            SpawnBulletHole(hit, ray);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction + new Vector3(x, y, 0), out hit))
+            {
+                gunImpact.transform.position = hit.point;
+                gunImpact.transform.forward = hit.normal;
+                gunImpact.Emit(1);
+
+                SpawnBulletHole(hit, ray);
+            }
         }
 
         equippedGun.bulletsLeft--;
-        equippedGun.bulletsShot--;
 
         Invoke("ResetShot", equippedGun.shootingCooldown);
-
-        if(equippedGun.bulletsShot > 0 && equippedGun.bulletsLeft > 0)
-        Invoke("Shoot", equippedGun.shootingCooldown);
     }
 
     void SpawnBulletHole(RaycastHit hit, Ray ray)
@@ -179,7 +202,7 @@ public class Hands : MonoBehaviour
 
         spawnedObject.transform.rotation = targetRotation;
         spawnedObject.transform.SetParent(bulletHoleContainer.transform);
-        spawnedObject.transform.Rotate(Vector3.forward, UnityEngine.Random.Range(0f, 360f));
+        spawnedObject.transform.Rotate(Vector3.forward, Random.Range(0f, 360f));
         Destroy(spawnedObject, destroyDelay);
     }
 
@@ -196,7 +219,16 @@ public class Hands : MonoBehaviour
     
     private void ReloadFinished()
     {
-        equippedGun.bulletsLeft = equippedGun.magSize;
+        if (Inventory[selectedGun].ammo >= Inventory[selectedGun].magSize - Inventory[selectedGun].bulletsLeft)
+        {
+            Inventory[selectedGun].ammo -= Inventory[selectedGun].magSize - Inventory[selectedGun].bulletsLeft;
+            Inventory[selectedGun].bulletsLeft = Inventory[selectedGun].magSize;
+        }
+        else
+        {
+            Inventory[selectedGun].bulletsLeft += Inventory[selectedGun].ammo;
+            Inventory[selectedGun].ammo = 0;
+        }
         reloading = false;
     }
 
